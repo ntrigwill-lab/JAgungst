16a6fe3169f9191ee201180f1c85366c274027b0
ntrigwill-lab <naufaltrigwill@gmail.com>
2025-08-20 20:19:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def predict_image(img: Image.Image):
 
 # Streamlit UI
 st.title("🌽 Klasifikasi Penyakit Daun Jagung")
-uploaded_file = st.file_uploader("🖼️ Upload gambar daun...", type=["jpg", "jpeg", "png", "bmp", "webp"])
+uploaded_file = st.file_uploader("🖼️ Upload gambar daun...", type=["jpg", "jpeg", "png", "bmp", "webp", "gif"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
